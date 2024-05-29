146601bc8f1d8c86935dcabc3e562d0293de4eae
Galaxy <ganli2012@gmail.com>
2024-05-29 00:05:48 -0700

    Change PrepareProTopoList: only if all processes in group are missing, raise FAIL_GET_PROC error.

--- util/pcerr.cpp
+++ util/pcerr.cpp
@@ -51,19 +51,8 @@ namespace pcerr {
     };
     static int warnCode = SUCCESS;
     static std::string warnMsg = "";
-
-    ProfErrorObj ProfErrorObj::profErrorObj;
-
-    ProfErrorObj& ProfErrorObj::GetInstance()
-    {
-        return profErrorObj;
-    }
-
-    void ProfErrorObj::SetProfError(const ProfError& profError)
-    {
-        this->errCode = profError->Code();
-        this->errMsg = profError->Msg();
-    }
+    static int errCode = SUCCESS;
+    static std::string errMsg = "";
 
     void New(int code)
     {
@@ -77,31 +66,35 @@ namespace pcerr {
 
     void New(int code, const std::string& msg)
     {
-        ProfError profError = std::make_shared<details::CodeStrMsgError>(code, msg);
-        ProfErrorObj::GetInstance().SetProfError(profError);
+        errCode = code;
+        errMsg = msg;
     }
 
     void SetWarn(int warn)
     {
-        warnCode = warn;
         auto findMsg = warnMsgs.find(warn);
         if (findMsg != warnMsgs.end()) {
-            warnMsg = findMsg->second;
+            SetWarn(warn, findMsg->second);
         } else {
-            warnMsg = "";
+            SetWarn(warn, "");
         }
     }
 
+    void SetWarn(int code, const std::string& msg)
+    {
+        warnCode = code;
+        warnMsg = msg;
+    }
 }  // namespace pcerr
 
 int Perrorno()
 {
-    return pcerr::ProfErrorObj::GetInstance().GetCode();
+    return pcerr::errCode;
 }
 
 const char* Perror()
 {
-    return pcerr::ProfErrorObj::GetInstance().GetMsg().c_str();
+    return pcerr::errMsg.c_str();
 }
 
 int GetWarn()
