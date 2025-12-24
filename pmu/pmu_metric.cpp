40b95ca808174d622033b130890dd79b2bc793de
echodo <echodo@noreply.gitcode.com>
2025-12-24 09:56:07 +0800

    fix two socket

--- pmu/pmu_metric.cpp
+++ pmu/pmu_metric.cpp
@@ -1342,6 +1342,7 @@ namespace KUNPENG_PMU {
         {5, {{0, 0}, {1, 1}, {2, 2}, {3, 3}}},
         {7, {{0, 4}, {1, 5}, {2, 6}, {3, 7}}},
     };
+
     static IdxMap DDRC_CHANNEL_MAP_HIPB = {
         {3, {{0, 0}, {2, 1}, {3, 2}, {5, 3}}},
         {1, {{0, 4}, {2, 5}, {3, 6}, {5, 7}}},
@@ -1352,8 +1353,10 @@ namespace KUNPENG_PMU {
     static IdxMap DDRC_CHANNEL_MAP_HIPG = {
             {0, {{0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}}},
             {1, {{0, 6}, {1, 7}, {2, 8}, {3, 9}, {4, 10}, {5, 11}}},
+            {8, {{0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}}},
+            {9, {{0, 6}, {1, 7}, {2, 8}, {3, 9}, {4, 10}, {5, 11}}},
     };
-
+    
     static unordered_map<int, IdxMap>
         DDRC_CHANNEL_MAP = {
             {HIPA, DDRC_CHANNEL_MAP_HIPA},
