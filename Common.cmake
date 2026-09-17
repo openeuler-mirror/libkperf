b6fb2e4f56fdb6c24f623cf9bb3008389fa0e650
wuying39 <wuying53@huawei.com>
2026-09-17 14:07:30 +0800

    modify: add compilation option

--- Common.cmake
+++ Common.cmake
@@ -13,17 +13,17 @@
 
 
 set(CMAKE_CXX_FLAGS_DEBUG " -O0 -g -Wall -pg -fno-gnu-unique -DDEBUG")
-set(CMAKE_CXX_FLAGS_RELEASE " -O3 -DNDEBUG -fstack-protector-all -Wl,-z,relro,-z,now -Wall -fPIE -s")
+set(CMAKE_CXX_FLAGS_RELEASE " -O3 -DNDEBUG -fstack-protector-all -D_FORTIFY_SOURCE=2 -Wl,-z,relro,-z,now -Wall -fPIE -s")
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     string(APPEND CMAKE_CXX_FLAGS_DEBUG   " -fno-gnu-unique")
     string(APPEND CMAKE_CXX_FLAGS_RELEASE " -fno-gnu-unique")
 endif()
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g")
-set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g -D_FORTIFY_SOURCE=2")
+set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG -D_FORTIFY_SOURCE=2")
 set(CMAKE_C_FLAGS_DEBUG " -O0 -g -Wall -pg -DDEBUG")
-set(CMAKE_C_FLAGS_RELEASE " -O3 -DNDEBUG -fstack-protector-all -Wl,-z,relro,-z,now -Wall -fPIE -s")
-set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 -g")
-set(CMAKE_C_FLAGS_MINSIZEREL "-Os -DNDEBUG")
+set(CMAKE_C_FLAGS_RELEASE " -O3 -DNDEBUG -fstack-protector-all -D_FORTIFY_SOURCE=2 -Wl,-z,relro,-z,now -Wall -fPIE -s")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 -g -D_FORTIFY_SOURCE=2")
+set(CMAKE_C_FLAGS_MINSIZEREL "-Os -DNDEBUG -D_FORTIFY_SOURCE=2")
 
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE " -Wl,-z,relro,-z,now,-z,noexecstack -pie -s")
 set(THIRD_PARTY ${PROJECT_TOP_DIR}/third_party)
